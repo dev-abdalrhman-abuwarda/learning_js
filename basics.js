9547a62d469325d094809084421fa75d82ff2589
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2025-04-12 17:29:44 +0200

    Js Function  and Array

--- basics.js
+++ basics.js
@@ -24,9 +24,58 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-*/
 
 let age = 31;
 age = 30;
 
 const birthYear = 2002;
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+
+// Function Declation 
+// You can call the Function declaration befor the declartion function
+const age1 = calcAge(2002);
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    return age;
+}
+
+// const age1 = calcAge(2002);
+// console.log(age1);
+
+
+// Function Expretion
+// You can not call at after declaration
+const calacAge2 = function (birthYear) {
+    return 2025 - birthYear;
+}
+
+const age2 = calacAge2(2002);
+
+// Arrow Function
+const calacAge3 = birthYear => 2025 - birthYear;
+const age3 = calacAge3(2002);
+console.log(age3);
+
+const yearUntilRetirment = (birthYear, firstName) => {
+    const age = 2025 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement}`;
+}
+
+console.log(yearUntilRetirment(2002, "Abdalrhman"));
+*/
+
+const frindes = ['Ahmed', 'Abdalrhman', 'mohammed', 'alaa'];
+const years = new Array[1990, 1995, 2000, 2002];
+console.log(frindes[0]);
+console.log(frindes[2]);
+console.log(frindes.length);
+console.log(frindes[frindes.length - 1]);
+
+frindes.push('Omar');
+console.log(frindes);
+
+
